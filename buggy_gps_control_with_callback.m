d608fc3afece402997a18318e02ad8731dbd5751
Mathieu Bresciani <mathieu.bresciani@epfl.ch>
2015-11-02 13:38:13 +0100

    Add PWM -> Speed/Steering conversion and brakes when hitting a key

--- buggy_gps_control_with_callback.m
+++ buggy_gps_control_with_callback.m
@@ -39,23 +39,55 @@ lat0 = msg.Latitude;
 lon0 = msg.Longitude;
 
 % Define control loop period
-loop_period = 0.050; 
+loop_period = 0.050;
+figure(1);
+xPos = 0;
+yPos = 0;
+h = plot(xPos,xPos, 'o');
+axis([-15 15 -15 15]);
+xlabel('Meters towards East');
+ylabel('Meters towards North');
 
-while 1
+global stop_car
+stop_car = 0;
+gcf
+set(gcf, 'KeyPressFcn', @myKeyPressFcn);
+
+while ~stop_car
     tic;
     
     % Check if new GNSS data is available
     if gps_new_data == 1
-        [x, y] = gps2carthesian(lat0, lon0, car_lat, car_lon);
+        [x, y] = latlon2carthesian(lat0, lon0, car_lat, car_lon);
         gps_new_data = 0;
+        xPos = [xPos x];
+        yPos = [yPos y];
+        set(h,'XData',xPos,'YData',yPos);
         d = sqrt(x*x+y*y);
         fprintf('\t Longitude:%3.6f \t Latitude:%3.6f \t Altitude:%3.6f\n', msg.Longitude, msg.Latitude, msg.Altitude);
         fprintf('Coordinates:  \n\t x: %2.2f (m)\t y: %2.2f (m)\t d: %2.2f (m)\n', x, y, d);
     end
     
     % Update car speed and steering angle
-    engine_msg.Data = 0;
-    steering_msg.Data = 0;
+    steeringAngle = -0.0;  % Between -0.54 (left) and 0.54 (right) in radians
+    carSpeed = 0;       % In m/s
+    
+    if carSpeed > 3
+        carSpeed = 3;
+    end
+    
+    steeringValue = -0.962*steeringAngle.^2 + 1.434*steeringAngle;
+    engineValue = 0.84*carSpeed+2.543;                            % Up to 10
+    
+    if carSpeed < 0.5
+        engineValue = 0;
+    end
+    % Failsafe
+    if engineValue > 6
+        engineValue = 6;
+    end
+    engine_msg.Data = engineValue-12.5;
+    steering_msg.Data = steeringValue-0.435;
 
     send(engine_pub, engine_msg)
     send(steering_pub, steering_msg)
@@ -71,3 +103,12 @@ while 1
     pause(loop_period - loop_t);
     % toc
 end
+
+% Stop
+engine_msg.Data = 0-12.5;
+steering_msg.Data = 0-0.435;
+
+send(engine_pub, engine_msg)
+send(steering_pub, steering_msg)
+
+rosshutdown;
