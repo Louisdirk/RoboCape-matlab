fea087536854a84b755809b9d11b370d2459bcef
Mathieu Bresciani <mathieu.bresciani@epfl.ch>
2015-11-24 17:44:34 +0100

    Add analysis plots for virtual arena output

---
+++ Data_Trajectory_Analysis/Analysis.m
@@ -0,0 +1,107 @@
+
+close all
+%% Extract measurements
+
+mask = isnan(ret{1}.measurements(1,:));
+j = 1;
+t = ret{1}.time;
+for i = 1:length(ret{1}.measurements(1,:))
+    if mask(i) == 0
+        meas(:,j) = ret{1}.measurements(:,i);
+        t_meas(j) = ret{1}.time(i);
+        j = j+1;
+    end
+end
+
+
+%% State observation evolution
+inputs = ret{1}.inputTrajectory;
+
+Ts = t(2)-t(1);
+% Steering model filter
+Fs = tf(1,[0.1 1]);
+Fz = c2d(Fs,Ts);
+inputsF(2,:) = filter(Fz.num{1}, Fz.den{1},inputs(2,:));
+
+% figure; plot(t,inputs(2,:),t,inputsF(2,:))
+
+gpsEst = ret{1}.observerStateTrajectory(1:2,:);
+vfEst = ret{1}.observerStateTrajectory(3,:);
+yawEst = ret{1}.observerStateTrajectory(4,:);
+steeringEst = ret{1}.observerStateTrajectory(5,:);
+
+% Plot GPS position
+% figure(1);
+% c = linspace(1,10,length(meas));
+% scatter(meas(1,:), meas(2,:), [], c);
+% hold all
+% quiver(meas(1,:), meas(2,:), 0.1*cos(meas(6,:)), 0.1*sin(meas(6,:)))
+% 
+% % Plot GPS estimate
+% plot(gpsEst(1,:), gpsEst(2,:), '.')
+% quiverDisplay = 1:10:length(gpsEst);
+% quiver(gpsEst(1,quiverDisplay), gpsEst(2,quiverDisplay), vfEst(quiverDisplay).*cos(yawEst(quiverDisplay)), vfEst(quiverDisplay).*sin(yawEst(quiverDisplay)))
+% title('Position');
+ui_plot_traj(t_meas, meas, t, gpsEst, vfEst, yawEst);
+
+figure(2);
+plot(t,vfEst);
+title('Estimated speed');
+xlabel('Time (s)');
+ylabel('Speed (m/s)');
+
+figure(3);
+plot(t,yawEst);
+hold all
+plot(t_meas,unwrap(meas(6,:)),'x');
+title('Yaw angle');
+xlabel('Time (s)');
+ylabel('Yaw angle (rad)');
+legend('Estimated', 'Measured');
+
+figure(4);
+plot(t,unwrap(steeringEst),t, inputs(2,:), t, inputsF(2,:));
+title('Steering angle');
+xlabel('Time (s)');
+ylabel('Steering angle (rad)');
+
+%% Imu readings
+figure(5);
+plot(t_meas,meas(3,:),'-*');
+hold all;
+plot(t_meas,meas(4,:), '-+');
+title('Accelerometers');
+xlabel('Time (s)');
+ylabel('Acceleration (m/s^2)');
+legend('X axis', 'Y axis');
+
+figure(6);
+plot(t_meas,meas(5,:));
+title('Gyroscope');
+xlabel('Time (s)');
+ylabel('Angular rate (rad/s)');
+legend('Z axis');
+
+%% Innovation
+yn = size(meas,1);
+inn = ret{1}.inn;
+
+figure(7);
+
+yNamesList = {...
+                'gpsX', ...
+                'gpsY', ...
+                'accX', ...
+                'accY', ...
+                'gyroZ', ...
+                'yaw'};
+
+for i = 1:yn
+    subplot(yn,1,i)
+    plot(t,inn(i,:));
+    ylabel(yNamesList{i});
+end
+
+xlabel('Time (s)');
+subplot(yn,1,1)
+title('Innovation');
---
+++ Data_Trajectory_Analysis/ui_plot_traj.m
@@ -0,0 +1,38 @@
+function ui_plot_traj(t_meas, meas, t_est, gpsEst, vfEst, yawEst)
+
+% Plot GPS measurements
+c = linspace(1,10,length(meas));
+h1 = scatter(meas(1,:), meas(2,:), [], c);
+hold all
+h2 = quiver(meas(1,:), meas(2,:), 0.1*cos(meas(6,:)), 0.1*sin(meas(6,:)));
+
+% Plot GPS estimate
+h3 = plot(gpsEst(1,:), gpsEst(2,:), '.');
+quiverDisplay = 1:10:length(gpsEst);
+h4 = quiver(gpsEst(1,quiverDisplay), gpsEst(2,quiverDisplay), vfEst(quiverDisplay).*cos(yawEst(quiverDisplay)), vfEst(quiverDisplay).*sin(yawEst(quiverDisplay)));
+title('Position');
+
+span = [1 t_est(end)];
+varname = 'Time';
+hs = uicontrol(...
+    'style','slider',...
+    'pos',[10 40 280 20],...
+    'min',span(1),...
+    'max',span(2),...
+    'value',span(2),...
+    'callback',@sliderupdate,...
+    'tag',varname);
+
+    function sliderupdate(a,b)
+        maxT = a.Value;
+        [cc, iMax_t] = min(abs(t_est-maxT));
+        [cc, iMax_t_meas] = min(abs(t_meas-maxT));
+        set(h1, 'XData', meas(1,1:iMax_t_meas), 'YData', meas(2,1:iMax_t_meas), 'CData', c(1:iMax_t_meas));
+        set(h2, 'XData', meas(1,1:iMax_t_meas), 'YData', meas(2,1:iMax_t_meas), 'UData', 0.1*cos(meas(6,1:iMax_t_meas)), 'VData', 0.1*sin(meas(6,1:iMax_t_meas)));
+        set(h3, 'XData', gpsEst(1,1:iMax_t), 'YData', gpsEst(2,1:iMax_t));
+        quiverDisplay = 1:10:length(iMax_t);
+        set(h4, 'XData', gpsEst(1,quiverDisplay), 'YData', gpsEst(2,quiverDisplay), 'UData', vfEst(quiverDisplay).*cos(yawEst(quiverDisplay)), 'VData', vfEst(quiverDisplay).*sin(yawEst(quiverDisplay)));
+        drawnow;
+    end
+
+end
