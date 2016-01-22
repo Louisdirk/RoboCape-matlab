0df2d81d208a83a7278f254a8f1fa1f50edc1b89
Mathieu Bresciani <mathieu.bresciani@epfl.ch>
2016-01-22 17:15:48 +0100

    Add link quality plots

--- Data_Trajectory_Analysis/Analysis.m
+++ Data_Trajectory_Analysis/Analysis.m
@@ -22,9 +22,13 @@ end
 
 %% State observation evolution
 inputs = ret{1}.inputTrajectory;
-inputs(2,:) = inputs(2,:);
-
 Ts = t(2)-t(1);
+
+% Velocity model filter
+Fs1 = tf(1,[1.8 1]);
+Fz1 = c2d(Fs1,Ts);
+inputsF(1,:) = filter(Fz1.num{1}, Fz1.den{1},inputs(1,:));
+
 % Steering model filter
 Fs = tf(1,[0.1 1]);
 Fz = c2d(Fs,Ts);
@@ -58,10 +62,14 @@ ui_plot_traj(t_meas, meas, t, gpsEst, vfEst, yawEst);
 
 % vfGps = dxGps*5; % GPS @ 5Hz
 figure(2);
-plot(t, inputs(1,:),t,vfEst);
+plot(t, inputs(1,:), t, inputsF(1,:),t,vfEst);
+hold all
+plot(t, vfEst+sigEst(3,:), 'g--',t,vfEst-sigEst(3,:), 'g--');
+hold off
 title('Forward speed');
 xlabel('Time (s)');
 ylabel('Speed (m/s)');
+legend('v_{ref}', 'v_{model}', 'v_{est}', '\sigma_{est}');
 
 figure(3);
 plot(t,yawEst);
@@ -74,12 +82,18 @@ ylabel('Yaw angle (rad)');
 legend('Estimated', 'Confidence interval', '', 'Measured');
 
 figure(4);
-steeringFromGyro = asin(meas(5,:)*l./1);
-plot(t, inputs(2,:), t, inputsF(2,:), t_meas, steeringFromGyro, t,unwrap(steeringEst));
+
+steeringFromGyro = asin(meas(5,:)*l./vfEst(~mask));
+% sqrt(var(steeringFromGyro-inputsF(2,~mask)))*180/pi
+
+plot(t, inputs(2,:), t, inputsF(2,:), t,unwrap(steeringEst), t_meas, steeringFromGyro);
+hold all
+plot(t, unwrap(steeringEst+sigEst(5,:)), 'g--', t, unwrap(steeringEst-sigEst(5,:)), 'g--')
+hold off
 title('Steering angle');
 xlabel('Time (s)');
 ylabel('Steering angle (rad)');
-legend('Desired', '1st order model', 'Value from gyro, speed and geometry', 'State estimate');
+legend('\delta_{ref}', '\delta_{model}' , '\delta_{est}', 'Value from gyro, speed and geometry', '\sigma_{est}');
 
 %% Imu readings
 figure(5);
@@ -105,22 +119,30 @@ inn = ret{1}.inn;
 figure(7);
 
 yNamesList = {...
-    'gpsX', ...
-    'gpsY', ...
-    'accX', ...
-    'accY', ...
-    'gyroZ', ...
-    'yaw'};
+    'GNSS_x', ...
+    'GNSS_y', ...
+    'Acc_x', ...
+    'Acc_y', ...
+    'Gyro_z', ...
+    'Compass'};
 
-for i = 1:yn
-    subplot(yn,1,i)
+% for i = 1:yn
+%     ax(i) = subplot(yn,1,i);
+%     stem(t,inn(i,:));
+%     ylabel(yNamesList{i});
+% end
+j = 0;
+for i = [1 2 5]
+    j = j +1;
+    ax(j) = subplot(3,1,j);
     stem(t,inn(i,:));
     ylabel(yNamesList{i});
 end
-
+xlim([0 50]);
+linkaxes(ax,'x');
 xlabel('Time (s)');
-subplot(yn,1,1)
-title('Innovation');
+% subplot(yn,1,1)
+% title('Innovation');
 
 %% Path from yaw estimate
 posFromYaw_mem = zeros(2,1);
@@ -144,7 +166,12 @@ xlabel('Time (s)');
 
 figure;
 plot(t,sqrt(ret{1}.observerStateTrajectory(6,:)))
-legend('xPos sqrt(covariance)');
+legend('xPos sqrt(var)');
+xlabel('Time (s)');
+
+figure;
+plot(t,sqrt(ret{1}.observerStateTrajectory(3,:)))
+legend('vf sqrt(var)');
 xlabel('Time (s)');
 
 %% Lyapunov variables
@@ -158,5 +185,33 @@ end
 plot(t,ret{1}.lyapVar(1,:),t,ret{1}.lyapVar(2,:),t,ret{1}.lyapVar(3,:),t,V)
 
 title('Lyapunov variables and function');
-legend('e1','e2','z',['1/2e' char(39) 'e + 1/2z^2']);
+hl = legend('$e_1$','$e_2$','$z$','$V$');
+set(hl, 'Interpreter','latex');
 xlabel('Time (s)');
+ylabel('Amplitude');
+
+%% Communication quality
+IMU_Tics = ~isnan(ret{1}.measurements(5,:));
+windowSize = 10;
+IMU_Density = conv(double(IMU_Tics), ones(1,windowSize), 'valid')/windowSize;
+figure;
+subplot(2,1,1);stem(t,IMU_Tics*100,'Color',[0.8 0.8 0.8]) % IMU
+hold all
+plot(t(1:end-(windowSize-1)),IMU_Density*100,'Color',[0 0 0]);
+hold off
+ylabel('Q_{imu} (%)');
+xlim([0 50]);
+
+GPS_Tics = ~isnan(ret{1}.measurements(1,:));
+GPS_Density = conv(double(GPS_Tics), ones(1,windowSize), 'valid')/(windowSize);
+subplot(2,1,2);stem(t,GPS_Tics*100,'Color',[0.8 0.8 0.8]) % GNSS
+hold all
+plot(t(1:end-(windowSize-1)),min(GPS_Density*100*3,100),'Color',[0 0 0]);
+hold off
+xlabel('Time (s)');
+ylabel('Q_{gnss} (%)');
+xlim([0 50]);
+
+% figure; plot(diff(t(GPS_Tics)))
+figure; histogram(diff(t(IMU_Tics)),30,'Normalization','probability')
+figure; histogram(diff(t(GPS_Tics)),10)
