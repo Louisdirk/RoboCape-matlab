fad8704ecb2e8d21a7980afa604033559ea36f47
Mathieu Bresciani <mathieu.bresciani@epfl.ch>
2016-01-22 17:51:54 +0100

    Add comments

--- car control/Trajectory generator/rajectory_circle_smoother3.m
+++ car control/Trajectory generator/rajectory_circle_smoother3.m
@@ -1,12 +1,21 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Author: Mathieu Bresciani
+% Description:
+%   This program used the path smoother / trajectory designer and simulates
+%   the calculated path. The parameters can be stored in order to use them
+%   in Virtual Arena (in runme.m).
 %
-% Draw trajectory
-%
+% Dependencies:
+%   - TrajectoryGenerator.m
+%   -   \ StraightTrajectory.m
+%   -   | CircularTrajectory
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 clear all
 close all
+clc
+
+%% Parameters
 
 radius = 1;
 aCircle = 2;                            % Maximum centripetal acceleration
@@ -27,23 +36,26 @@ vCircle = sqrt(aCircle*radius);         % Max speed in curve
 
 pv = [0; vCircle; vCircle; vCircle; 0]; % Checkpoint velocities
 
-%%
-clear all
-load('traj2_Osterrechichring_Austria_params');
-aMax = 1;                               % Maximum forward acceleration
-vSat = 3;
+%% Use old path and modify some parameters
 
-radius = 1;
-aCircle = 1;                            % Maximum centripetal acceleration
+% clear all
+% load('traj2_Osterrechichring_Austria_params');
+% aMax = 1;                               % Maximum forward acceleration
+% vSat = 3;
+% 
+% radius = 1;
+% aCircle = 1;                            % Maximum centripetal acceleration
+% 
+% vCircle = sqrt(aCircle*radius);         % Max speed in curve
+% 
+% pv = [0; ones(nCheckpoints-2,1)*vCircle; 0]; % Checkpoint velocities
 
-vCircle = sqrt(aCircle*radius);         % Max speed in curve
-
-pv = [0; ones(nCheckpoints-2,1)*vCircle; 0]; % Checkpoint velocities
-%%
+%% Build path, smoothes it and computes trajectories parameters
 
 traj = TrajectoryGenerator(p,pv,radius,aCircle,aMax,vSat);
 
 %% Prepare plots
+
 figure
 h = plot(0,0,'.');
 title('Path');
@@ -55,15 +67,18 @@ axis equal
 %% Run simulation
 
 t = 0:0.06:50;
-% load('timeVA');
-% t = timeVA;
+
 for k = 1:length(t)
-%     [pSim(k,:), vSim(k,:), aSim(k,:)] = traj.getTrajFromTime(t(k));
+    % Get trajectory
     pSim(k,:) = traj.getPosFromTime(t(k))';
     vSim(k,:) = traj.getVelFromTime(t(k))';
     aSim(k,:) = traj.getAccFromTime(t(k))';
+    
+    % Plot
     set(h,'XData',pSim(:,1),'YData',pSim(:,2));
     drawnow;
+    
+    % Store projected trajectory
     trajProj = traj.getTrajProj();
     pSimProj(k) = trajProj(1);
     vSimProj(k) = trajProj(2);
@@ -71,7 +86,7 @@ for k = 1:length(t)
     vRef(k) = aSimProj(k)*1.8 + vSimProj(k);
 end
 
-%%
+%% Display trajectory
 
 figure;
 ax1 = subplot(3,1,1);
